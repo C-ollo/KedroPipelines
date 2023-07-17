fdb9eeff4a52f9a56017cdcd9fa637434d7abdee
collins Kimutai <collinskimutai72@gmail.com>
2023-07-17 05:48:47 +0000

    build pipeline

---
+++ src/openrepair/__init__.py
@@ -0,0 +1,5 @@
+"""
+OpenRepair analysis
+"""
+
+__version__ =  "0.1.0"
---
+++ src/openrepair/settings.py
@@ -0,0 +1,15 @@
+import polars as pl
+from kedro.config import OmegaConfigLoader
+
+# if not OmegaConf.has_resolver("pl"):
+#     OmegaConf.register_new_resolver("pl", lambda attr: getattr(pl, attr))
+
+
+CONFIG_LOADER_CLASS = OmegaConfigLoader
+
+# See https://github.com/kedro-org/kedro/issues/2583
+CONFIG_LOADER_ARGS = {
+    "config_patterns": {
+        "catalog": ["catalog.yml", "**/catalog.yml"],
+    }
+}
